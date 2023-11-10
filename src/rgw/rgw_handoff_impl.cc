39170aa286b365d0f1fb36bec82ba0aa592914d7
André Lucas <andre@ae-35.com>
2023-11-10 13:21:47 +0000

    Prep: Set up the files for PIMPL.
    We're going to PIMPL-ise HandoffHelper, to limit the blast radius of
    gRPC. Why? Otherwise including "rgw_handoff.h" will pull in all the gRPC
    includes, which (a) explodes; and (b) is a waste of compile time.

---
+++ src/rgw/rgw_handoff_impl.cc
@@ -0,0 +1,16 @@
+/**
+ * @file rgw_handoff_impl.cc
+ * @author André Lucas (alucas@akamai.com)
+ * @brief Implementation for rgw::HandoffHelper (PIMPL pattern).
+ * @version 0.1
+ * @date 2023-11-10
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#include "rgw_handoff_impl.h"
+
+namespace rgw {
+
+} // namespace rgw
