2363ed54f5896f8aee1a246e00330d67e9fcab40
André Lucas <andre@ae-35.com>
2024-08-09 00:17:22 +0100

    Handoff: RGWSetBucketVersioning.

--- src/rgw/rgw_op.cc
+++ src/rgw/rgw_op.cc
@@ -2792,11 +2792,17 @@ void RGWGetBucketVersioning::execute(optional_yield y)
 
 int RGWSetBucketVersioning::verify_permission(optional_yield y)
 {
-  auto [has_s3_existing_tag, has_s3_resource_tag] = rgw_check_policy_condition(this, s, false);
-  if (has_s3_resource_tag)
-    rgw_iam_add_buckettags(this, s);
+  // HANDOFF: Visited.
+  if (s->handoff_authz->enabled()) {
+    return s->handoff_helper->verify_permission(this, s, rgw::IAM::s3PutBucketVersioning, y);
 
-  return verify_bucket_owner_or_policy(s, rgw::IAM::s3PutBucketVersioning);
+  } else {
+    auto [has_s3_existing_tag, has_s3_resource_tag] = rgw_check_policy_condition(this, s, false);
+    if (has_s3_resource_tag)
+      rgw_iam_add_buckettags(this, s);
+
+    return verify_bucket_owner_or_policy(s, rgw::IAM::s3PutBucketVersioning);
+  }
 }
 
 void RGWSetBucketVersioning::pre_exec()
