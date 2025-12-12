afa71932397189cac787668f23e51d4824b80412
André Lucas <andre@ae-35.com>
2025-12-12 10:22:57 +0000

    Further tweak log macros.
    Work correctly with empty args using C++20's __VA_OPT__.
    
    Remove a little redundancy.
    
    Add non-conditional variants. We always use fmt::format(), so let's be
    consistent. Use COND_LOG* for debug, and LOG* for errors and warnings.

--- src/rgw/driver/mdoffload/rgw_sal_mdoffload.cc
+++ src/rgw/driver/mdoffload/rgw_sal_mdoffload.cc
@@ -25,28 +25,35 @@
 // certain level. Used to avoid building log message strings unnecessarily.
 #define LOG_ENABLED(cct, level) ((cct)->_conf->subsys.should_gather(dout_subsys, (level)))
 // Variant taking a dpp instead of a cct.
-#define LOG_ENABLED_D(dpp, level) LOG_ENABLED((dpp)->get_cct(), level)
+#define LOG_ENABLED_PFX(dpp, level) LOG_ENABLED((dpp)->get_cct(), level)
 // Global version.
 #define LOG_ENABLED_G(level) LOG_ENABLED(g_ceph_context, level)
 
-#define COND_LOG(cct, level, msg, ...)                                         \
-  do {                                                                         \
-    if (LOG_ENABLED(cct, level)) {                                             \
-      ldout(cct, level) << fmt::format(FMT_STRING(msg), __VA_ARGS__) << dendl; \
-    }                                                                          \
+#define LOG(cct, level, msg, ...)                                                          \
+  do {                                                                                     \
+    ldout(cct, level) << fmt::format(FMT_STRING(msg) __VA_OPT__(, ) __VA_ARGS__) << dendl; \
   } while (0)
-#define COND_LOG_D(dpp, level, msg, ...)                                           \
-  do {                                                                             \
-    if (LOG_ENABLED_D(dpp, level)) {                                               \
-      ldpp_dout(dpp, level) << fmt::format(FMT_STRING(msg), __VA_ARGS__) << dendl; \
-    }                                                                              \
+
+#define LOG_PFX(dpp, level, msg, ...)                                                          \
+  do {                                                                                         \
+    ldpp_dout(dpp, level) << fmt::format(FMT_STRING(msg) __VA_OPT__(, ) __VA_ARGS__) << dendl; \
   } while (0)
-#define COND_LOG_G(level, msg, ...)                                                       \
-  do {                                                                                    \
-    if (LOG_ENABLED_G(level)) {                                                           \
-      ldout(g_ceph_context, level) << fmt::format(FMT_STRING(msg), __VA_ARGS__) << dendl; \
-    }                                                                                     \
+
+#define LOG_G(level, msg, ...) LOG(g_ceph_context, level, msg __VA_OPT__(, ) __VA_ARGS__)
+
+#define COND_LOG(cct, level, msg, ...)                                                       \
+  do {                                                                                       \
+    if (LOG_ENABLED(cct, level)) {                                                           \
+      ldout(cct, level) << fmt::format(FMT_STRING(msg) __VA_OPT__(, ) __VA_ARGS__) << dendl; \
+    }                                                                                        \
   } while (0)
+#define COND_LOG_PFX(dpp, level, msg, ...)                                                       \
+  do {                                                                                           \
+    if (LOG_ENABLED_PFX(dpp, level)) {                                                           \
+      ldpp_dout(dpp, level) << fmt::format(FMT_STRING(msg) __VA_OPT__(, ) __VA_ARGS__) << dendl; \
+    }                                                                                            \
+  } while (0)
+#define COND_LOG_G(level, msg, ...) COND_LOG(g_ceph_context, level, msg __VA_OPT__(, ) __VA_ARGS__)
 
 namespace akamai::grpcutil {
 rgw::sal::Attrs attrs_from_proto(const ::google::protobuf::Map<std::string, std::string>& proto_attrs)
